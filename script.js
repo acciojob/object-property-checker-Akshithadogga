2ccfbcfc4670ef295c33ada32644d2109bf129a4
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-11-03 16:07:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let arr=Objcet.keys(sampleObject);
+	if(arr.inclue(key)){
+		return true;
+	}
+	else{
+		retuen false;
+	}
 }
 
 // Do not change the code below
